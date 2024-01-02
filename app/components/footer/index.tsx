9666e2485bc01d566fe172f5217dc1df998fc62b
Bruno Mathias <bm.grey@outlook.com>
2024-01-02 15:59:32 -0300

    Formulário e Rodapé Layout

---
+++ app/components/footer/index.tsx
@@ -0,0 +1,14 @@
+import { IoMdHeart } from 'react-icons/io'
+ 
+export const Footer = () => {
+    return(
+       <footer className='h-14 w-full flex items-center justify-center bg-gray-950'>
+            <span className='flex items-center gap-1.5 text-xs sm:text-sm font-mono text-gray-400'>
+                Made with
+                <IoMdHeart className='text-emerald-400 ' size={13}/>
+                 by
+                 <strong className='font-medium'> Bruno Mathias</strong>
+            </span>
+         </footer> 
+    )
+}
