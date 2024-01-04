3f9c5d308e2f777dbbeb308ce256342ca8539c43
Bruno Mathias <bm.grey@outlook.com>
2024-01-04 17:33:48 -0300

    Ajustes

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,44 @@ import { HeroSection } from "./components/pages/home/hero-section";
 import { HigliightedProjects } from "./components/pages/home/higlighted-projects";
 import { Knowtechs } from "./components/pages/home/know-techs";
 import { WorkExperience } from "./components/pages/home/work-experience";
+import { fetchHygraphQuery } from "./utils/fetch-hygraph-query";
+
+const getPageData = async () => {
+  const query = `
+    query PageInfoQuery {
+      page(where: {slug: "home"}) {
+        introduction {
+          raw
+        }
+      technologies {
+        name
+      }
+      profilePicture {
+        url
+      }
+      socials {
+        url
+        iconSvg
+      }
+      knownTechs {
+        iconSvg
+        name
+        startDate
+      }
+      
+    }
+    
+  }
+`
+  return fetchHygraphQuery(query);
+};
 
 export default async function Home() {
+  
+  const response = await getPageData();
+
+  console.log(response);
+
   return (
     <>
       <HeroSection />
@@ -11,5 +47,5 @@ export default async function Home() {
       <HigliightedProjects />
       <WorkExperience />
     </>
-  )
+  );
 }
--- app/utils/fetch-hygraph-query.ts
+++ app/utils/fetch-hygraph-query.ts
@@ -1,20 +1,18 @@
-
-
 export const fetchHygraphQuery = async (query: string) => {
-    const response = await fetch(process.env.HYGRAPH_URL!, {
+    const response = await fetch(process.env.NEXT_PUBLIC_HYGRAPH_URL!, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             Accept: 'application/json',
-            Authorization: `Bearer ${process.env.HYGRAPH_TOKEN}`
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_HYGRAPH_TOKEN}`
         },
         body: JSON.stringify({ query }),
         next: {
             revalidate: 60 * 60 * 24 //24h
         }
-    })  
-
+    }) 
+ 
     const { data } = await response.json()
-
+ 
     return data
-}
+ }
