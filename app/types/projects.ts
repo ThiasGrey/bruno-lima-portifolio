c065ca4970dfed83b709ec8016e6d2d2fc14e033
Bruno Mathias <bm.grey@outlook.com>
2024-01-11 01:11:51 -0300

    Buscando Projetos do Hygraph

--- app/types/projects.ts
+++ app/types/projects.ts
@@ -1,38 +1,38 @@
 import { RichTextContent } from "@graphcms/rich-text-types";
 
 export type KnowTech = {
-    iconSvg: string,
-    name: string,
-    startDate: string
-}
+  iconSvg: string;
+  name: string;
+  startDate: string;
+};
 
-export type ProjectSection= {
-    title: string
-    image: {
-        url:string
-    }  
-}
+export type ProjectSection = {
+  title: string;
+  image: {
+    url: string;
+  };
+};
 
 export type Technology = {
-    name: string
-  }
+  name: string;
+};
 
 export type Project = {
-    slug: string
-    thumbnail: {
-      url: string
-    }
-    title: string
-    shortDescription: string
-    technologies: Technology[]
-    pageThumbnail: {
-      url: string
-    }
-    sections: ProjectSection[]
-    description: {
-      raw: RichTextContent
-      text: string
-    }
-    liveProjectUrl?: string
-    githubUrl?: string
-  }
+  slug: string;
+  thumbnail: {
+    url: string;
+  };
+  title: string;
+  shortDescription: string;
+  technologies: Technology[];
+  pageThumbnail: {
+    url: string;
+  };
+  sections: ProjectSection[];
+  description: {
+    raw: RichTextContent;
+    text: string;
+  };
+  liveProjectUrl?: string;
+  githubUrl?: string;
+};
