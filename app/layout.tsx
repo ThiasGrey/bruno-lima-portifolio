c1946a0d2c69a65571f8dcabe1ef2935a32a5599
Bruno Mathias <bm.grey@outlook.com>
2024-01-25 17:58:20 -0300

    Vercel Analytcs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,49 +1,46 @@
-import { Inter, IBM_Plex_Mono } from 'next/font/google'
-import './globals.css'
-import { ReactNode } from 'react'
-import { Header } from './components/header'
-import { ContactForm } from './components/contact-form'
-import { Footer } from './components/footer'
-import { BackToTop } from './components/back-to-top'
-import { Toaster } from './components/toaster'
-import { SpeedInsights } from '@vercel/speed-insights/next';
-
+import { Inter, IBM_Plex_Mono } from "next/font/google";
+import "./globals.css";
+import { ReactNode } from "react";
+import { Header } from "./components/header";
+import { ContactForm } from "./components/contact-form";
+import { Footer } from "./components/footer";
+import { BackToTop } from "./components/back-to-top";
+import { Toaster } from "./components/toaster";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
-  title:  {
-    default: 'Bruno M. | DEV',
-    template: '%s | BM Dev',
+  title: {
+    default: "Bruno M. | DEV",
+    template: "%s | BM Dev",
   },
   icons: [
     {
-      url: '/favicon.svg',
-    }
-  ],
-    description: 'Bruno M. | Desenvolvedor Front-end e UI Designer',
-    openGraph: {
-      images: [
-        {
-          url: '/images/og-image.png',
-          width: 1200,
-          height: 630,
-          
-        },
-      ],
+      url: "/favicon.svg",
     },
-    
-  
-}
+  ],
+  description: "Bruno M. | Desenvolvedor Front-end e UI Designer",
+  openGraph: {
+    images: [
+      {
+        url: "/images/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+};
 
 const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-})
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
 
 const plexMono = IBM_Plex_Mono({
-  variable: '--font-plex-mono',
-  subsets: ['latin'],
-  weight: ['400', '500'],
-})
+  variable: "--font-plex-mono",
+  subsets: ["latin"],
+  weight: ["400", "500"],
+});
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
@@ -55,8 +52,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         {children}
         <ContactForm />
         <Footer />
-         <SpeedInsights />
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
-  )
+  );
 }
